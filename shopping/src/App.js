a89725fb61cf7f42d5e1f841df25383ff5abd724
dnjsvltm201311681 <dnjsvltm201311681@gmail.com>
2022-01-10 23:59:20 +0900

    bootrstrap start

--- shopping/src/App.js
+++ shopping/src/App.js
@@ -1,7 +1,12 @@
 import "./App.css";
+import NavbarContainer from "./Navbar";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <NavbarContainer />
+    </div>
+  );
 }
 
 export default App;
