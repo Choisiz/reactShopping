f423d91fd450ec4e1d8b580457959aa62bc123c5
dnjsvltm201311681 <dnjsvltm201311681@gmail.com>
2022-01-16 23:56:37 +0900

    변경

--- shopping/src/App.js
+++ shopping/src/App.js
@@ -4,13 +4,16 @@ import NavbarContainer from "./Navbar";
 import Jumbotron from "./Jumbotron/Jumbotron";
 import Detail from "./Component/Detail";
 import { Link, Route, Switch } from "react-router-dom";
+import Shoes from "./Component/Shoes";
+import { useState } from "react";
 function App() {
+  const [shoes, setShoes] = useState();
   return (
     <div className="App">
       <NavbarContainer />
       <Jumbotron />
       <Switch>
-        <Route exact path="/" component={Main} />
+        <Route exact path="/" reder={() => <Main />} />
         <Route path="/detail" component={Detail} />
         <Route path="/:id">
           <div>hellddo</div>
